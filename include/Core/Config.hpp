f08d6738e1f7f2c5044effcf36b13ea8e0647ce6
夕木 夜間 <finn.yaki.0rz@gmail.com>
2025-10-28 14:12:19 +0900

    update: update config file

--- include/Core/Config.hpp
+++ include/Core/Config.hpp
@@ -5,7 +5,7 @@ namespace Core {
     // 393×852
     constexpr int kWindowWidth = 480;
     constexpr int kWindowHeight = 720;
-    constexpr char kWindowTitle[] = "タイムリミット：残された選択";
+    constexpr char kWindowTitle[] = "5143_タイムリミット：残された選択";
 
     constexpr int INPUT_LOCK_FRAME = 30;
     constexpr int COUNT_TIME = (3 * 60);
